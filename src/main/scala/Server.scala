5b3bbf0c98f7dd97c4083a301a35a7116b3b837d
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-11-14 11:26:06 +0300

    create a host and a port ot bind the server

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,3 +1,13 @@
-object Server extends App {
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 
+import scala.concurrent.ExecutionContext
+
+object Server extends App {
+  val host = "0.0.0.0"
+  val port = 9000
+
+  implicit val system: ActorSystem = ActorSystem("helloworld")
+  implicit val executor: ExecutionContext = system.dispatcher
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 }
